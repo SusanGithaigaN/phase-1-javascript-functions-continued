53feb61f253727f930e78bd48820d30646b8767a
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-25 12:15:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+//Function saturdayFun definition.
+function saturdayFun(target = "roller-skate") {
+  return `This Saturday, I want to ${target}!`;
+}
+// Function mondayWork definition.
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// Function wrapAdjective definition.
+function wrapAdjective(type = "*"){
+  const flairing = function(adjective = "special"){
+    return `You are ${type}${adjective}${type}!`;
+  };
+return flairing;
+ 
+}
